cf02f9d25912f15163d830cec0bfdd4c6f93d1fc
elaine-caed <77505253+elaine-caed@users.noreply.github.com>
2021-04-23 14:22:29 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EX4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { " Sunday ", " Monday ", " Tuesday ", " Wednesday ", " Thirsday ", " Friday ", " Saturday "};
+        for (int i = 0; i < daysOfWeek.Length; i++)
+           Console.WriteLine("AS[{0}] = {1}", i, daysOfWeek[i]);
+        Console.ReadKey();
+        }
+    }
+}
